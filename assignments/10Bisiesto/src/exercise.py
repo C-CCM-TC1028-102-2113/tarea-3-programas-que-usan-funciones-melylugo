a433cc71f751e7f1adc795337eb7838dc49fd434
melylugo <88989879+melylugo@users.noreply.github.com>
2021-08-29 17:02:49 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,19 @@
+def es_bisiesto (year):
+    if year%4==0:
+        if year%100==0 and year%400==0:
+            bisiesto=True
+        elif year%100==0 and year%400!=0:
+            bisiesto=False
+        else:
+            bisiesto=True
+    else:
+        bisiesto=False
+    return bisiesto
 
 def main():
-    #escribe tu código abajo de esta línea
+    a=int(input())
+    z=es_bisiesto(a)
+    print(z)
     pass
 
 if __name__=='__main__':
